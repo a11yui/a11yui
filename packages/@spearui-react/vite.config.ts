3d5d93b29257464f701b714ab9fa3e61da832ab4
Morgan Spencer <morgan@spencercreative.co>
2022-05-15 19:28:32 -0400

    React: fix build script

--- packages/@spearui-react/vite.config.ts
+++ packages/@spearui-react/vite.config.ts
@@ -1,10 +1,17 @@
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      copyDtsFiles: false,
+    }),
+    react(),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/components/index.ts'),
