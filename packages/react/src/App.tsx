23e449e0b3b0677aaecb43a28141b1e3fc047949
Morgan Spencer <morgan@spencercreative.co>
2022-05-16 08:09:35 -0400

    React: add testing packages

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <h2>Accordion</h2>
 
-      <Accordion multiselect>
+      <Accordion>
         <Accordion.Item>
           <Accordion.Toggle>Title</Accordion.Toggle>
           <Accordion.Content>Content</Accordion.Content>
